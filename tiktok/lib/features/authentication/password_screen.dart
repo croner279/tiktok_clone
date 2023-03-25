2d920f93fb443d55d7569765c29a7faef5617a06
yongha.kim <croner279@gmail.com>
2023-03-26 08:11:48 +0900

    2.7 Password screen

--- tiktok/lib/features/authentication/password_screen.dart
+++ tiktok/lib/features/authentication/password_screen.dart
@@ -1,10 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/features/authentication/birthday_screen.dart';
+import 'package:tiktok/features/authentication/widgets/form_button.dart';
+import '../../constants/sizes.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  final TextEditingController _PasswordController = TextEditingController();
+
+  String _password = "";
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _PasswordController.addListener(() {
+      setState(() {
+        _password = _PasswordController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _PasswordController.dispose(); // usernamecontroller 및 연관된 이벤트 리스너들 전부 삭제
+    super.dispose(); // initState와는 반대로, super.dispose는 끝에.
+  }
+
+  bool _isPasswordValid() {
+    return _password.isNotEmpty && _password.length > 8;
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  } // Email 작성칸 외에 다른 곳을 누르거나 하면 키보드가 해제되게 함(unfocus)
+
+  void _onSubmit() {
+    if (!_isPasswordValid()) return;
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const BirthdayScreen(),
+        ));
+  }
+
+  void _onClearTap() {
+    _PasswordController.clear();
+  }
+
+  void _toggleObscureText() {
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Sign up",
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                "Password",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                obscureText: _obscureText,
+                controller: _PasswordController,
+                keyboardType: TextInputType.emailAddress,
+                onEditingComplete: _onSubmit,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min, //Row가 최소한의 필요한 공간만 가져가도록.
+                    children: [
+                      GestureDetector(
+                        onTap: _onClearTap,
+                        child: FaIcon(
+                          FontAwesomeIcons.solidCircleXmark,
+                          color: Colors.grey.shade500,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                      Gaps.h16,
+                      GestureDetector(
+                        onTap: _toggleObscureText,
+                        child: FaIcon(
+                          _obscureText
+                              ? FontAwesomeIcons.eye
+                              : FontAwesomeIcons.eyeSlash,
+                          color: Colors.grey.shade500,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  hintText: "Make it strong!",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v10,
+              const Text(
+                'Your password must have:',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Gaps.v10,
+              Row(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.circleCheck,
+                    size: Sizes.size20,
+                    color: _isPasswordValid()
+                        ? Colors.green
+                        : Colors.grey.shade300,
+                  ),
+                  Gaps.h5,
+                  const Text('8 to 20 characters')
+                ],
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmit,
+                child: FormButton(
+                  disabled: !_isPasswordValid(),
+                ),
+              )
+            ], //비었으면 당연히 disabled이고, 정규식 유효성 검사를 통과해도 null이 나오니까 통과 못해도 disabled임.
+          ),
+        ),
+      ),
+    );
   }
 }
