7ca3e2c845cc2c135b9907944832b64ce7b266df
yongha.kim <croner279@gmail.com>
2023-03-26 21:04:28 +0900

    3.3 Tutorial Screen

--- tiktok/lib/features/onboarding/interests_screen.dart
+++ tiktok/lib/features/onboarding/interests_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/onboarding/interest_button.dart';
+import 'package:tiktok/features/onboarding/tutorial_screen.dart';
 
 const interests = [
   "Daily Life",
@@ -43,75 +45,98 @@ const interests = [
   "Home & Garden",
 ];
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
+
+  @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool _showTitle = false;
+
+  void _onScroll() {
+    if (_scrollController.offset > 110) {
+      if (_showTitle) {
+        return; // 이 부분 매우 중요함. 110 넘어가면 그냥 계속 상단바 타이틀 보여주면 되는데, 이 문장 없으면 offset 변할때마다 setState가 끊임없이 이뤄짐.
+      }
+      //즉, offset값이 110 넘어가는 그 지점에서만 딱 한번 showTitle 값을 변경시키는 거임.
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
+
+  void _onNextTap() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const TutorialScreen(),
+        ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Choose your interest'),
+        title: AnimatedOpacity(
+          opacity: _showTitle ? 1 : 0,
+          duration: const Duration(milliseconds: 300),
+          child: const Text('Choose your interest'),
+        ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: Sizes.size24,
-            right: Sizes.size24,
-            bottom: Sizes.size16,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v32,
-              const Text(
-                'Choose your interest',
-                style: TextStyle(
-                  fontSize: Sizes.size36,
-                  fontWeight: FontWeight.bold,
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: Sizes.size24,
+              right: Sizes.size24,
+              bottom: Sizes.size16,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                const Text(
+                  'Choose your interest',
+                  style: TextStyle(
+                    fontSize: Sizes.size36,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Gaps.v20,
-              const Text(
-                'Get better video recommendations',
-                style: TextStyle(
-                  fontSize: Sizes.size20,
+                Gaps.v20,
+                const Text(
+                  'Get better video recommendations',
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                  ),
                 ),
-              ),
-              Gaps.v64,
-              Wrap(
-                runSpacing: 15,
-                spacing: 15,
-                children: [
-                  for (var interest
-                      in interests) //collection for을 통해 수십개의 container들을 한번에 렌더링!! Super cool!
-                    //근데 사실 웹툰 앱처럼 List가 너무 길고, 다양한 속성이 있어서 Widget이 무거우면 사용자가 일단 보는 부분만 렌더링 하는게 바람직함 collection for 가 아니라 ListViewBuilder로.
-                    //여기는 뭐 리스트 길지도 않고 속성도 별것없어서(컨테이너, 글씨굵기, 색깔 등..) 그냥 collection for로 가즈아
-                    Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: Sizes.size12,
-                          horizontal: Sizes.size12,
-                        ),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(Sizes.size32),
-                            border: Border.all(
-                                color: Colors.black.withOpacity(0.1)),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.05),
-                                blurRadius: 5,
-                                spreadRadius: 5,
-                              )
-                            ]),
-                        child: Text(
-                          interest,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ))
-                ],
-              ),
-            ],
+                Gaps.v48,
+                Wrap(runSpacing: 15, spacing: 15, children: [
+                  for (var interest in interests)
+                    InterestButton(interest: interest)
+                ]),
+              ],
+            ),
           ),
         ),
       ),
@@ -124,17 +149,20 @@ class InterestsScreen extends StatelessWidget {
             left: Sizes.size24,
             right: Sizes.size24,
           ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: Sizes.size20),
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-            ),
-            child: const Text(
-              'Next',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: Sizes.size16,
+          child: GestureDetector(
+            onTap: _onNextTap,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: Sizes.size20),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: const Text(
+                'Next',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: Sizes.size16,
+                ),
               ),
             ),
           ),
---
+++ tiktok/lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: const SafeArea(
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v52,
+                    Text(
+                      'Watch cool videos!',
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      'Videos are personalized for you based on what you watch, like, and share.',
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v52,
+                    Text(
+                      'Follow!',
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      'Videos are personalized for you based on what you watch, like, and share.',
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v52,
+                    Text(
+                      ' cool videos!',
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      'Videos are personalized for you based on what you watch, like, and share.',
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size48,
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TabPageSelector(
+                  color: Colors.white,
+                  selectedColor: Colors.black38,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
