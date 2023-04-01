ea515500394a6535b9a4edb05d5a3f005d98513a
yongha.kim <croner279@gmail.com>
2023-04-02 08:58:47 +0900

    7.2 TabBar

--- tiktok/lib/features/discover/discover_screen.dart
+++ tiktok/lib/features/discover/discover_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "Live",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Discover'),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text('Discover'),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+            isScrollable: true,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size14,
+            ),
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [
+              for (var tab
+                  in tabs) //PreferredSizedWidget을 넣을 수 있고, TabBar가 그것임.
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          for (var tab in tabs)
+            Center(
+              child: Text(
+                tab,
+                style: const TextStyle(fontSize: Sizes.size16),
+              ),
+            )
+        ]),
       ),
     );
   }
