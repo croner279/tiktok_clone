ea7f4f138e4f38b75602da9b66fb78ceef74667c
yongha.kim <croner279@gmail.com>
2023-04-02 10:16:37 +0900

    7.4 Grid Item

--- tiktok/lib/features/discover/discover_screen.dart
+++ tiktok/lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 final tabs = [
@@ -55,13 +57,61 @@ class DiscoverScreen extends StatelessWidget {
               crossAxisCount: 2,
               crossAxisSpacing: Sizes.size10,
               mainAxisSpacing: Sizes.size10,
-              childAspectRatio: 9 / 16,
+              childAspectRatio: 9 / 20,
             ),
-            itemBuilder: (context, index) => Container(
-              color: Colors.teal,
-              child: Center(
-                child: Text("$index"),
-              ),
+            itemBuilder: (context, index) => Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 9 / 16,
+                  child: FadeInImage.assetNetwork(
+                      fit: BoxFit.cover,
+                      placeholder: "assets/images/placeholder.jpg",
+                      image:
+                          "https://plus.unsplash.com/premium_photo-1667428818562-fc8379f23bfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"),
+                ),
+                Gaps.v10,
+                const Text(
+                  "This is a very long caption for my wooltok that i am uploading just now currently loading",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v8,
+                DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 13,
+                        backgroundImage: NetworkImage(
+                            "https://images.unsplash.com/photo-1570654621852-9dd25b76b38d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
+                      ),
+                      Gaps.h4,
+                      const Expanded(
+                        child: Text(
+                          "My Avartata is going to be very very long",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Gaps.h4,
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size16,
+                        color: Colors.grey.shade600,
+                      ),
+                      Gaps.h2,
+                      const Text("2.5M")
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
           for (var tab in tabs.skip(1))
