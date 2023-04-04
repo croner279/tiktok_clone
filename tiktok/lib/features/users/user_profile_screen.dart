b4dbedff4d5d6982373daab593f4b91a4b3b9319
yongha.kim <croner279@gmail.com>
2023-04-05 06:35:37 +0900

    10.6 TabBar

--- tiktok/lib/features/users/user_profile_screen.dart
+++ tiktok/lib/features/users/user_profile_screen.dart
@@ -13,124 +13,204 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: const Text('울쨩몬'),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const FaIcon(FontAwesomeIcons.gear, size: Sizes.size20))
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              const CircleAvatar(
-                  radius: 50,
-                  //foregroundImage: NetworkImage(url),
-                  child: Text('울쨩이')),
-              Gaps.v20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "@pearlisgood",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size16,
-                    ),
-                  ),
-                  Gaps.h5,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    color: Colors.blue.shade300,
-                    size: Sizes.size16,
-                  ),
-                ],
-              ),
-              Gaps.v24,
-              SizedBox(
-                height: Sizes.size36,
-                child: Row(
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text('울쨩몬'),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const FaIcon(FontAwesomeIcons.gear, size: Sizes.size20))
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                    radius: 50,
+                    //foregroundImage: NetworkImage(url),
+                    child: Text('울쨩이')),
+                Gaps.v20,
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Column(
-                      children: [
-                        const Text(
-                          "96",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size16,
-                          ),
-                        ),
-                        Gaps.h5,
-                        Text(
-                          "Following",
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
+                    const Text(
+                      "@pearlisgood",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size16,
+                      ),
                     ),
-                    //VerticalDivider는 부모 위젯의 height를 가져다 쓰기 때문에, 상위 위젯인 Row를 SizedBox로 감싸서 height를 지정해줘야 함.
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey.shade400,
-                      indent: Sizes.size12,
-                      endIndent: Sizes.size12,
-                    ),
-                    Column(
-                      children: [
-                        const Text(
-                          "10M",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size16,
-                          ),
-                        ),
-                        Gaps.h5,
-                        Text(
-                          "Followers",
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
-                    ),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey.shade400,
-                      indent: Sizes.size12,
-                      endIndent: Sizes.size12,
-                    ),
-                    Column(
-                      children: [
-                        const Text(
-                          "197M",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size16,
-                          ),
-                        ),
-                        Gaps.h5,
-                        Text(
-                          "Likes",
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      color: Colors.blue.shade300,
+                      size: Sizes.size16,
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
-        )
-      ],
+                Gaps.v24,
+                SizedBox(
+                  height: Sizes.size36,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            "96",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size16,
+                            ),
+                          ),
+                          Gaps.h5,
+                          Text(
+                            "Following",
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      //VerticalDivider는 부모 위젯의 height를 가져다 쓰기 때문에, 상위 위젯인 Row를 SizedBox로 감싸서 height를 지정해줘야 함.
+                      VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        color: Colors.grey.shade400,
+                        indent: Sizes.size12,
+                        endIndent: Sizes.size12,
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "10M",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size16,
+                            ),
+                          ),
+                          Gaps.h5,
+                          Text(
+                            "Followers",
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        color: Colors.grey.shade400,
+                        indent: Sizes.size12,
+                        endIndent: Sizes.size12,
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            "197M",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size16,
+                            ),
+                          ),
+                          Gaps.h5,
+                          Text(
+                            "Likes",
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                FractionallySizedBox(
+                  widthFactor: 0.33,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Sizes.size12,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: const BorderRadius.all(
+                            Radius.circular(Sizes.size4))),
+                    child: const Text(
+                      'Follow',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Gaps.v14,
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.size32,
+                  ),
+                  child: Text(
+                    "All highlights of pearlisgood. Jin Yeo Wool's home sweet home, I wonder how it would look",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Gaps.v14,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size12,
+                    ),
+                    Gaps.h4,
+                    Text(
+                      "https:jinyeowool-archive.com",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                //TabBar는 Controller가 필요함. DefaultTabController를 사용한다.
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.symmetric(
+                          horizontal: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 1,
+                  ))),
+                  child: const TabBar(
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicatorColor: Colors.black,
+                    labelPadding: EdgeInsets.only(
+                      bottom: Sizes.size10,
+                    ),
+                    labelColor: Colors.black,
+                    tabs: [
+                      Padding(
+                        //indicatorSize: TabBarIndicatorSize.label 에 따라 사이즈 조정. 아이콘에 패딩을 많이 줘서 라벨 자체를 키웠음.
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: Icon(Icons.grid_4x4_rounded),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: FaIcon(FontAwesomeIcons.heart),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
